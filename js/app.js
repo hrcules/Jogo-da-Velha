a5988599cf309c53368af3ae6b2354f9b7725513
Hércules Costa <dev.hrcules@gmail.com>
2025-07-08 15:09:40 -0300

    feat: criando lógica do jogo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,44 @@
-console.log("Olá");
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+// contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+// adicionando o evento de click aos boxes
+for (let i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener("click", function () {
+    let el = checkEl(player1, player2);
+
+    //verifica se tem x ou o
+    if (this.childNodes.length == 0) {
+      let cloneEl = el.cloneNode(true);
+
+      this.appendChild(cloneEl);
+
+      //computar jogada
+      if (player1 == player2) {
+        player1++;
+      } else {
+        player2++;
+      }
+    }
+  });
+}
+
+function checkEl(p1, p2) {
+  if (p1 == p2) {
+    // x
+    el = x;
+  } else {
+    // o
+    el = o;
+  }
+
+  return el;
+}
